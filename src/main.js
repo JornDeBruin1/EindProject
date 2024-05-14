358a95e5f3429a15c8e9768aaa4d56df19b8b461
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2024-05-14 13:15:00 +0200

    added tailwind and renamed pages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
+import './theme/tailwind.css';
 import { IonicVue } from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
@@ -38,7 +38,6 @@ import '@ionic/vue/css/display.css';
 
 
 /* Theme variables */
-import './theme/variables.css';
 
 const app = createApp(App).use(IonicVue).use(router);
 
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,9 +3,9 @@
 		<ion-tabs>
 			<ion-router-outlet></ion-router-outlet>
 			<ion-tab-bar>
-				<ion-tab-button tab="tab1" href="/tabs/tab1">
-					<ion-icon aria-hidden="true" :icon="triangle" />
-					<ion-label>Tab 1</ion-label>
+				<ion-tab-button tab="Home" href="/tabs/home">
+					<!-- <ion-icon aria-hidden="true" :icon="triangle" /> -->
+					<ion-label class="text-[32px]">Home</ion-label>
 				</ion-tab-button>
 
 				<ion-tab-button tab="tab2" href="/tabs/tab2">
